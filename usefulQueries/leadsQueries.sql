5c2bfaa76de0269c3dd083a0bff05209f956f24e
Developer <developer@IM-M11-408.local>
2018-09-20 14:27:14 -0500

    added leadsQueries.sql

--- usefulQueries/leadsQueries.sql
+++ usefulQueries/leadsQueries.sql
@@ -1,10 +1,10 @@
 CREATE TABLE leads (
     lead_id serial primary key,
-    insertion_type char(50) not null,
-    email char(50) not null,
+    insertion_type varchar(50) not null,
+    email varchar(50) not null,
     phone int,
     contact_id int not null,
-    target_provider char(50) not null,
+    target_provider varchar(50) not null,
     user_to_syncs_id int not null,
     creation_date  date default(NOW()),
     last_mod_date date, 
